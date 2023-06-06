39254c18f84b9e955eb97a3e36a0d19016203b90
RamKashyap2050 <ramkashyap2050@gmail.com>
2023-06-06 19:53:54 -0400

    Navbar Updated Logo Added

--- frontend/src/components/AdminNavbar.jsx
+++ frontend/src/components/AdminNavbar.jsx
@@ -1,26 +1,67 @@
-import React from 'react';
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
 const AdminNavbar = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("AdminData");
+    navigate("/loginadmin");
+  };
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-smoky-white">
-      <a className="navbar-brand" href="/">Admin Dashboard</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#admin-navbar-nav" aria-controls="admin-navbar-nav" aria-expanded="false" aria-label="Toggle navigation">
+      <a className="navbar-brand" href="/">
+        EasyShop
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#admin-navbar-nav"
+        aria-controls="admin-navbar-nav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="admin-navbar-nav">
         <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="/adminDashboard">Dashboard</a>
-          </li>
           <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="/" id="admin-settings-dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a
+              className="nav-link dropdown-toggle"
+              href="/"
+              id="admin-settings-dropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
               Settings
             </a>
-            <div className="dropdown-menu" aria-labelledby="admin-settings-dropdown">
-              <a className="dropdown-item" href="/adminprofile">Profile</a>
+            <div
+              className="dropdown-menu"
+              aria-labelledby="admin-settings-dropdown"
+            >
+              <a className="dropdown-item" href="/adminprofile">
+                Profile
+              </a>
+              <a className="dropdown-item" href="/adminprofile">
+                Add a Category
+              </a>
+              <a className="dropdown-item" href="/adminprofile">
+                View Orders
+              </a>
+              <a className="dropdown-item" href="/adminprofile">
+                Add a New Product
+              </a>
               <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="/adminLogout">Logout</a>
+              <a className="dropdown-item" onClick={handleLogout}>
+                Logout
+              </a>
             </div>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/adminDashboard">
+              Dashboard
+            </a>
           </li>
         </ul>
       </div>
--- frontend/src/pages/AdminProfile.jsx
+++ frontend/src/pages/AdminProfile.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import AdminNavbar from "../components/AdminNavbar";
-
+import Logo from "../Logo.jpg";
+import "../styles/AdminProfile.css";
 const AdminProfile = () => {
   const adminDataString = localStorage.getItem("AdminData");
   const adminData = adminDataString ? JSON.parse(adminDataString) : null;
@@ -16,7 +17,17 @@ const AdminProfile = () => {
   return (
     <div>
       <AdminNavbar />
-      <div style={{ maxWidth: "75%", margin: "4rem auto" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img src={Logo} alt="hello" className="admin-dashboard-photo" />
+      </div>
+
+      <div style={{ maxWidth: "60%", margin: "4rem auto" }}>
         <div className="card">
           <div className="card-body">
             <h1 className="card-title">Admin Profile</h1>
