2cbdfdf7f45ee15dd252620cd4a4ff67e21fa2bd
RamKashyap2050 <ramkashyap2050@gmail.com>
2023-06-06 13:46:06 -0400

    Initialization Done

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const path = require("path");
+// console.log(express)
+const connectDB = require("./config/db");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const cors = require("cors");
+const errorHandler = require("./middlewares/errorHandler");
+
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const methodOverride = require("method-override");
+const fileupload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const expressAsyncHandler = require("express-async-handler");
+PORT = process.env.PORT || 5001;
+connectDB();
+
+const app = express();
+app.use(fileupload());
+app.use(cors());
+app.use(express.json());
+app.use(methodOverride("_method"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+app.use("/Users", require("./routes/Userroutes"));
+app.use("/Admin", require("./routes/Adminroutes"));
+
+console.log(process.env.NODE_ENV);
+
+
+
+
+
+app.use(errorHandler);
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
