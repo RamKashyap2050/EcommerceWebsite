4f94e20cba6b3b314097210290728fc673baaa66
RamKashyap2050 <ramkashyap2050@gmail.com>
2023-06-30 19:33:56 -0400

    Latest Build

--- backend/server.js
+++ backend/server.js
@@ -43,13 +43,6 @@ if (process.env.NODE_ENV === "production") {
   app.get("/", (req, res) => res.send("Please Activate Production"));
 }
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3030");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
